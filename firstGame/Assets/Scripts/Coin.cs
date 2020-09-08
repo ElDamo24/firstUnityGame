242af8f12ec644c695a22bedae21e6ac074f2f15
Damian Lobos <damianlobos24@gmail.com>
2020-09-08 17:36:29 -0300

    add empty project

---
+++ firstGame/Assets/Scripts/Coin.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Security.Cryptography;
+using System.Threading;
+using UnityEngine;
+
+public class Coin : MonoBehaviour
+{
+    private float speed;
+    private playerScript playerScore; 
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerScore = FindObjectOfType<playerScript>();
+        speed = 50f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.RotateAround(transform.position, Vector3.down, speed * Time.deltaTime);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            Destroy(this.gameObject);
+            playerScore.score += 5;
+        }
+    }
+}
---
+++ firstGame/Assets/Scripts/playerScript.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using UnityEngine;
+
+public class playerScript : MonoBehaviour
+{
+    private float horizontalMovement;
+    private float verticalMovement;
+    [SerializeField]
+    private float speedMovement;
+    private float jumpVelocity;
+    private bool onTheFloor;
+    private Rigidbody rb;
+    public int score;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        onTheFloor = true;
+        speedMovement = 7f;
+        jumpVelocity = 100f;
+        score = 0;
+}
+
+    // Update is called once per frame
+    void Update()
+    {
+        horizontalMovement = Input.GetAxis("Horizontal");
+        verticalMovement = Input.GetAxis("Vertical");
+        transform.Translate(new Vector3(horizontalMovement, 0, verticalMovement) * speedMovement * Time.deltaTime);
+
+        if (Input.GetKeyDown(KeyCode.Space) && onTheFloor)
+        {
+            rb.velocity = new Vector3(0, jumpVelocity * Time.deltaTime, 0);
+            onTheFloor = false;
+        }
+
+        if (Input.GetKey(KeyCode.LeftShift))
+        {
+            speedMovement = 10f;
+        }
+        else
+        {
+            speedMovement = 7f;
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        onTheFloor = true;
+    }
+}
